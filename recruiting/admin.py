5da5cd42fd6955682f89018b2d63cdd214bb9134
yogesh <django.work@gmail.com>
2021-11-16 13:39:39 +0530

    admin and gitignore changes

--- recruiting/admin.py
+++ recruiting/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Candidate, Skill, Interview, CTC
 from django.utils.safestring import mark_safe
+from django.urls import reverse
+from django.utils.http import urlencode
+from django.utils.html import format_html
 
 
 class CTCInline(admin.TabularInline):
@@ -21,20 +24,31 @@ class SkillAdmin(admin.ModelAdmin):
 @admin.register(Candidate)
 class CandidateAdmin(admin.ModelAdmin):
 
-    list_display = ('full_name', 'email', 'mobile', 'cv', 'status',)
+    list_display = ('full_name', 'email', 'mobile', 'status', 'view_interview_link',)
     list_editable = ('experience', 'status')
     search_fields = ('email', 'full_name', 'mobile')
     list_filter = ('skills', 'status')
 
     list_editable = ('status',)
 
-    inlines = [CTCInline, InterviewInline]
+    inlines = [CTCInline]
+
+    def view_interview_link(self, obj):
+        count = obj.interviews.count()
+        url = (
+            reverse("admin:recruiting_interview_changelist")
+            + "?"
+            + urlencode({"interviews__id": f"{obj.id}"})
+        )
+        return format_html('<a href="{}">{} Interview</a>', url, count)
+
+    view_interview_link.short_description = "Interview"
 
 
 @admin.register(Interview)
-class CandidateAdmin(admin.ModelAdmin):
+class InterviewAdmin(admin.ModelAdmin):
 
     list_display = ('candidate', 'time', 'status', 'user', 'rating',)
-    list_editable = ('time', 'user')
+    list_editable = ('status',)
     search_fields = ('candidate', 'remark')
-    list_filter = ('time', 'status')
+    list_filter = ('status',)
